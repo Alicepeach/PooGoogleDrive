8c08e6643003844da66243bda4bd7227727d3d2f
AlicePeach <alicepeachmelocoton2@gmail.com>
2018-05-29 00:02:25 -0500

    Subiendo proyecto

---
+++ ProyectoPOOArchivos/src/Cliente/Modelo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+/**
+ *
+ * @author alice
+ */
+public class Modelo {
+    private String user;
+    private String password;
+    
+    //Constructor por defecto
+    public Modelo(){
+    }
+    
+}
---
+++ ProyectoPOOArchivos/src/proyectopooarchivos/ProyectoPOOArchivos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopooarchivos;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
+/**
+ *
+ * @author alice
+ */
+public class ProyectoPOOArchivos {
+    
+public static void main(String[] args) {
+
+/*		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+		int returnValue = jfc.showOpenDialog(null);
+		// int returnValue = jfc.showSaveDialog(null);
+
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = jfc.getSelectedFile();
+			System.out.println(selectedFile.getAbsolutePath());
+		}
+
+	}
+  */  
+    }
+}
